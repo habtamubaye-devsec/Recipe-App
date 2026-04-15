00204eea3948daa183c62da68b4e85ac43c5a80c
habtamubaye-devsec <habtamubaye2127@gmail.com>
2026-04-15 21:44:27 +0300

    fix on mobile app and update image paths and react dependencies across projects

--- apps/mobile/app/(tabs)/explore.tsx
+++ apps/mobile/app/(tabs)/explore.tsx
@@ -57,8 +57,8 @@ export default function TabTwoScreen() {
           <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to provide files for
           different screen densities
         </ThemedText>
-        <Image
-          source={require('@/assets/images/react-logo.png')}
+          <Image
+            source={require('../../assets/images/react-logo.png')}
           style={{ width: 100, height: 100, alignSelf: 'center' }}
         />
         <ExternalLink href="https://reactnative.dev/docs/images">
